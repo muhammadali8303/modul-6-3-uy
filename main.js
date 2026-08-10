66188409dd799733c9fe4d9ceea952cbcbff313e
muhammadali8303 <muhammadaliqaxxorov92@gmail.com>
2026-08-10 23:33:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+//boolean 10
+
+// let a =40;
+// a >= 10 && a <= 99 ? console.log("a soni ikki xonali") : console.log("a soni ikki xonali emas")
+// a % 2 === 0 ? console.log("a soni juft") : console.log("a soni toq");
+
+ //boolean 11
+
+//   let a =121;
+//  a >= 99 && a <= 999 ? console.log("a soni uch xonali") : console.log("a soni uch xonali emas")
+//  a % 2 === 1 ? console.log("a soni toq") : console.log("a soni juft");
+
+//boolean 12
+
+// let A = 5;
+// let B = 12;
+// let C = -5;
+
+
+// (A + B === 0) || (B + C === 0) || (A + C === 0) ? console.log("qarama qarshilik bor") : console.log("qarama qarshilik yoq");
+
+//boolean 13
+
+// let a = 3;
+// let b = 4;
+// let c = 6;
+// a<b && b < c ? console.log("raqamlar ozib boruvchi joylashgan") : console.log("raqamlar osib boruvchi emas")
+
+// boolean 14
+
+// let a = 3;
+// let b = 2;
+// let c = 1;
+// a<b && b < c || a> b && b> c ? console.log("raqamlar osib boruvchi yoki kamayib boruvchi joylashgan joylashgan") : console.log("raqamlar osib boruvchi yoki kamayib boruvchi emas")
+
+// boolean 15
+
+// let a = 6;
+// let b = 4;
+// let c = 6;
+// a == c ? console.log("raqamlar oyna") : console.log("raqamlar oyna emas");
+
